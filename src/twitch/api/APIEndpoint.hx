ddf43b5c3a09a5f0e1e6f3391f7cdd464cc50247
Felix R <felicitusneko@duck.com>
2022-08-06 18:12:04 -0300

    docs, remove elements from old endpoint system

--- src/twitch/api/APIEndpoint.hx
+++ src/twitch/api/APIEndpoint.hx
@@ -6,30 +6,41 @@ import haxe.Json;
 
 @:generic
 typedef APIResponse<R> = {
+	/** The HTTP return code supplied by the API endpoint. **/
 	var ?code:Int;
+	/** The data returned by the API endpoint, if any. **/
 	var ?data:R;
 	var ?date_range:{
 		var started_at:String;
 		var ended_at:String;
 	};
+	/** The total number of entries. **/
 	var ?total:Int;
+	/** The sum of all of your subscription costs. [Learn More](https://dev.twitch.tv/docs/eventsub/manage-subscriptions/#subscription-limits) **/
 	var ?total_cost:Int;
+	/** The maximum total cost that you may incur for all subscriptions you create. **/
 	var ?max_total_cost:Int;
+	/** A cursor value, to be used in a subsequent request to specify the starting point of the next set of results. **/
 	var ?pagination:{
+		/** The cursor used to page results. **/
 		var cursor:String;
 	};
+	/**
+		A templated URL. Use the values from `id`, `format`, `scale`, and `theme_mode` to replace the like-named placeholder strings
+		in the templated URL to create a CDN (content delivery network) URL that you use to fetch the emote. For information about what
+		the template looks like and how to use it to fetch emotes, see [Emote CDN URL format](https://dev.twitch.tv/docs/irc/emotes#cdn-template).
+	**/
 	var ?template:String;
+	/**
+		The current number of subscriber points earned by this broadcaster. Points are based on the subscription tier of each user that subscribes
+		to this broadcaster. For example, a Tier 1 subscription is worth 1 point, Tier 2 is worth 2 points, and Tier 3 is worth 6 points. The
+		number of points determines the number of emote slots that are unlocked for the broadcaster (see
+		[Subscriber Emote Slots](https://help.twitch.tv/s/article/subscriber-emote-guide#emoteslots)).
+	**/
 	var ?points:Int;
 }
 
-@:abstract
-@:noCompletion
-abstract class APIEndpoint {
-	public static var scopeRequired:Null<String>;
-	public static var oauthRequired:Bool;
-	public static var method:HttpMethod;
-	public static var endpoint:String;
-
+class APIEndpoint {
 	@:generic
 	public static function call<R>(method:HttpMethod, endpoint:String, client:Client, ?query:Dynamic, ?data:Dynamic):APIResponse<R> {
 		if (query != null && !Reflect.isObject(query)) throw new Exception("Query data is not an object");
@@ -39,7 +50,7 @@ abstract class APIEndpoint {
 			for (key in Reflect.fields(query))
 				if (Reflect.field(query, key) != null)
 					queryMap.set(key, Reflect.field(query, key));
-		
+
 		var apidata = client.call(method, endpoint, queryMap, data != null ? Json.stringify(data) : null);
 
 		var retval:APIResponse<R> = {};
