d676b69944783fb68457934b811904cf86cbb520
Felix R <felicitusneko@duck.com>
2022-08-30 01:33:07 -0300

    add Charity collection and endpoint also fixed a couple of definition errors

---
+++ src/twitch/api/Charity.hx
@@ -0,0 +1,9 @@
+package twitch.api;
+
+import haxe.http.HttpMethod;
+import twitch.api.APIEndpoint;
+
+/** Endpoints pertaining to charity campaigns. **/
+@:build(twitch.api.APIBuilder.build("charity"))
+class Charity {
+}
