9ab899eb0b033173d60ac0a6c1de22d7f85d8054
Felix R <felicitusneko@duck.com>
2022-08-30 02:40:33 -0300

    add GetChanneliCalendar

--- src/twitch/api/Schedule.hx
+++ src/twitch/api/Schedule.hx
@@ -3,11 +3,7 @@ package twitch.api;
 import haxe.http.HttpMethod;
 import twitch.api.APIEndpoint;
 
-/**
-  Endpoints pertaining to the stream schedule and its segments.
-
-  This currently excludes `GetChanneliCalendar`, as it returns plain text rather than a JSON object.
-**/
+/** Endpoints pertaining to the stream schedule and its segments. **/
 @:build(twitch.api.APIBuilder.build("schedule"))
 class Schedule {
 }
